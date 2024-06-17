55523fd9a0dcbe977c5a89110739319642340e62
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-06-17 13:33:12 +0800

    Add module,service, and controller for each feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmConfigModule } from './config/typeorm/typeorm.module';
+import { UsersModule } from './features/users/users.module';
+import { PostsModule } from './features/posts/posts.module';
+import { FollowModule } from './features/follow/follow.module';
+import { LikesModule } from './features/likes/likes.module';
+import { LikeController } from './features/like/like.controller';
 
 @Module({
-  imports: [TypeOrmConfigModule],
-  controllers: [],
+  imports: [
+    TypeOrmConfigModule,
+    UsersModule,
+    PostsModule,
+    FollowModule,
+    LikesModule,
+  ],
+  controllers: [LikeController],
   providers: [],
 })
 export class AppModule {}
