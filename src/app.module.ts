072cd3c5fc9ca223d41d94245af62462e8c40644
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-06-17 14:52:16 +0800

    Remove follow and like feature folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmConfigModule } from './config/typeorm/typeorm.module';
 import { UsersModule } from './features/users/users.module';
 import { PostsModule } from './features/posts/posts.module';
-import { FollowModule } from './features/follow/follow.module';
-import { LikesModule } from './features/likes/likes.module';
 
 @Module({
-  imports: [
-    TypeOrmConfigModule,
-    UsersModule,
-    PostsModule,
-    FollowModule,
-    LikesModule,
-  ],
+  imports: [TypeOrmConfigModule, UsersModule, PostsModule],
   controllers: [],
   providers: [],
 })
