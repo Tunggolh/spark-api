82008266d04c3eced4409fd8fa26249183b6c83c
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-06-17 17:05:55 +0800

    Add user controller

--- src/features/users/users.controller.ts
+++ src/features/users/users.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from 'src/database/entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { FollowUserDto } from './dto/follow-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<User> {
+    return this.userService.findOne(id);
+  }
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.userService.create(createUserDto);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @Post(':id/follow')
+  async followUser(
+    @Param('id') id: number,
+    @Body() followUserDto: FollowUserDto,
+  ) {
+    return this.userService.followUser(followUserDto);
+  }
+
+  @Delete(':id/follow')
+  async unfollowUser(
+    @Param('id') id: number,
+    @Body() followUserDto: FollowUserDto,
+  ) {
+    return this.userService.unfollowUser(followUserDto);
+  }
+}
