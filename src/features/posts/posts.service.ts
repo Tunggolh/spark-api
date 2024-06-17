5b7ec3a8f18160a81358a9c26db9b76e8858b760
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-06-17 15:29:16 +0800

    Add initial post service

--- src/features/posts/posts.service.ts
+++ src/features/posts/posts.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from 'src/database/entities/post.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>,
+  ) {}
+
+  async findAll(): Promise<Post[]> {
+    return this.postRepository.find({ order: { id: 'desc' } });
+  }
+}
