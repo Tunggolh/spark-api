0f6e0571018423d2a7d7ccf77007886731aa7303
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-06-17 15:57:59 +0800

    Add retrieve, create, and update for post service

--- src/features/posts/posts.service.ts
+++ src/features/posts/posts.service.ts
@@ -1,16 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Post } from 'src/database/entities/post.entity';
-import { Repository } from 'typeorm';
+import { DeepPartial, Repository } from 'typeorm';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { CreatePostDto } from './dto/create-post.dto';
+import { User } from 'src/database/entities/user.entity';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class PostsService {
   constructor(
     @InjectRepository(Post)
     private readonly postRepository: Repository<Post>,
+    private readonly userService: UsersService,
   ) {}
 
   async findAll(): Promise<Post[]> {
     return this.postRepository.find({ order: { id: 'desc' } });
+  }
+
+  async findOne(id: number): Promise<Post> {
+    const post = await this.postRepository.findOne({ where: { id } });
+
+    if (!post) {
+      throw new NotFoundException(`Post #${id} not found`);
+    }
+
+    return post;
+  }
+
+  async create(createPostDto: CreatePostDto): Promise<Post> {
+    const author = await this.userService.findOne(createPostDto.author);
+
+    const newPost: DeepPartial<Post> = {
+      content: createPostDto.content,
+      author,
+    };
+
+    return await this.postRepository.save(newPost);
+  }
+
+  async update(id: number, updatePostDto: UpdatePostDto): Promise<Post> {
+    const post = await this.findOne(id);
+    const updatedPost = Object.assign(post, updatePostDto);
+
+    return await this.postRepository.save(updatedPost);
   }
 }
