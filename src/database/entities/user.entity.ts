689a0786d5405b98608904a4d895742e9fa6b4b5
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-06-17 13:53:21 +0800

    Add user entity

---
+++ src/database/entities/user.entity.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @Column({ nullable: true })
+  bio: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
+}
